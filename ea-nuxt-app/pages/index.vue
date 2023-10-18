fe3c4ebbefada9f8ffa7d4d90813a81550a4c416
Matthew J. Collett <matthew.collett12@gmail.com>
2023-10-18 13:01:36 -0300

    Update index.vue

--- ea-nuxt-app/pages/index.vue
+++ ea-nuxt-app/pages/index.vue
@@ -12,7 +12,7 @@
       </section>
 
       <section class="text-center">
-        <nuxt-link to="/counter-status" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">View Counter Status</nuxt-link>
+        <nuxt-link to="/status" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">View Counter Status</nuxt-link>
       </section>
     </main>
   </div>
